65a9f753e3bd3570e243a6b8499566c0012d446e
liuysh <liuysh@sugon.com>
2023-12-23 12:42:05 +0800

    add

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <li class="item" @click="handleDelete"> {{ content }}</li>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: ['content', 'index'],
+    methods: {
+        handleDelete() {
+            this.$emit('handleDeleteParent',this);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.item{
+    color: green;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <input v-model="message" placeholder="edit me" />
+        <button @click="handleSubmit">提交</button>
+        <button @click="handleClear">清空</button>
+        <ul>
+            <todo-item v-for="(item, index) of list" :content="item" :index="index" :key="index" @handleDeleteParent="handleDeleteParent"></todo-item>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+import TodoItem from './TodoItem.vue';
+export default {
+    name: 'TodoList',
+    components: {
+        'todo-item': TodoItem
+    },
+    data() {
+        return {
+            message: 'https://blog.csdn.net/liuyunshengsir',
+            list: [],
+        }
+    },
+    methods: {
+        handleSubmit() {
+            //alert(this.message);
+            this.list.push(this.message);
+        },
+        handleClear() {
+            this.list = [];
+        },
+        handleDeleteParent(node) {
+            alert("删除节点"+node.index+":"+node.content);
+            this.list.splice(node.index,1);
+        }
+    }
+}
+</script>
